0f54fa715fac69881776d086f37e69d60ac9f224
kazunari-kamichika <155711273+kazunari-kamichika@users.noreply.github.com>
2024-06-06 23:57:51 +0900

    課題1-9　提出

--- 1-9/src/test1.java
+++ 1-9/src/test1.java
@@ -1,4 +1,27 @@
 
 public class test1 {
 
+	public static void main(String[] args) {
+		
+//		1.数字の5と9の変数を定義し、足した数字を出力しなさい。
+		
+		int i = 5;
+		int s = 9;
+		
+		System.out.print(i+s);
+		
+//		2.64ビット整数の「1111111111」を定義し2倍した値を出力しなさい。
+		
+		long t = 1111111111;
+		
+		System.out.print(t*2);
+		
+//		3.boolean型の変数の値に不正な文字列比較を入れてfalseを出力してください。
+		
+		 boolean nam = false;
+		 
+		 System.out.print(nam);
+		
+		
+	}
 }
