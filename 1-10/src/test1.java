c74a6dde0f87ae1b46e30dac27e6d39ef68d2572
kazunari-kamichika <155711273+kazunari-kamichika@users.noreply.github.com>
2024-07-19 23:07:17 +0900

    課題1-10 提出

--- 1-10/src/test1.java
+++ 1-10/src/test1.java
@@ -1,5 +1,7 @@
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 
 public class test1 {
 
@@ -46,20 +48,20 @@ public class test1 {
 
 		//問題4:この配列"numd"をコピーした配列名"new_numd"を作り"new_numd"の中身を出力してください。
 		int[] numd = { 1, 3, 5, 7, 9 };
-		
-		int[] new_numd = Arrays.copyOf(numd, 5);	
-		
-		System.out.println(Arrays.toString(new_numd)); 
+
+		int[] new_numd = Arrays.copyOf(numd, 5);
+
+		System.out.println(Arrays.toString(new_numd));
 
 		//改行
 		System.out.println("問５");
 
 		//問題5:この配列"nume"を昇順に並び替えて出力しなさい。
 		int[] nume = { 6, 2, 8, 1, 9 };
-		
+
 		Arrays.sort(nume);
-					
-			System.out.println(Arrays.toString(nume));
+
+		System.out.println(Arrays.toString(nume));
 
 		//改行
 		System.out.println("問６");
@@ -67,35 +69,55 @@ public class test1 {
 		//問題6:配列"numf"に要素を1つ(10)追加した配列"new_numf"を作成し出力しなさい。
 		int[] numf = { 1, 3, 5, 7, 9 };
 		int[] new_numf = new int[6];
-		
+
 		System.arraycopy(numf, 0, new_numf, 0, 5);
-		
-		new_numf[5]=10;
-			
-		System.out.println(Arrays.toString(new_numf));	
-		
-		
-		
-		
+
+		new_numf[5] = 10;
+
+		System.out.println(Arrays.toString(new_numf));
 
 		//改行
 		System.out.println("問７");
-
+		
 		//問題7:Map"car"に「キー："honda"　バリュー"ホンダ",キー："toyota"　バリュー："トヨタ",キー："subaru"　バリュー："スバル"」の要素を追加してください
 
+		
+		Map<String,String> car = new HashMap <String,String>();
+		
+		car.put("honda","ホンダ");
+		car.put("toyota","トヨタ");
+		car.put("subaru","スバル");
+		
+		
 		//改行
 		System.out.println("問８");
 
 		//問題8:Map"car"の「キー："toyota"」のバリューを出力してください。
+		
+		 System.out.println(car.get("toyota"));
 
 		//改行
 		System.out.println("問９");
 
 		//問題9:Map"car"のキーとバリューを全て出力してください。※for文で行うこと
+		
+		for (String key : car.keySet()) {
+			System.out.println(key);
+		}
+		
 		//改行
 		System.out.println("問１０");
 
 		//問題10:Map"car"のキーとバリューを全て出力してください。※forEach文で行うこと
+		
+
+		car.forEach((k, v) -> {
+		    System.out.println(k);
+		    System.out.println(v);
+		});
+
+		car.forEach((key, value) -> System.out.println(key + " : " + value));
 
 	}
+	
 }
